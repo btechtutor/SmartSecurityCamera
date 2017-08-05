c47536c3c7ea22866f0e81bc4932b554f2593829
Mustafa Aldemir <mustafax.aldemir@intel.com>
2017-08-06 00:25:27 +0300

    publishes over mqtt and saves snapshots

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #https://software.intel.com/en-us/articles/opencv-at-the-edge-counting-people
 
 #!/usr/bin/env python
-from WebcamVideoStream import WebcamVideoStream
+from SmartCamera import SmartCamera
 import cv2
 import time
 
@@ -9,22 +9,24 @@ from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-def gen(vs):
+def gen(sc):
     count = 1
     while True:
-        frame = vs.readDetections()
+        frame = sc.getFrameWidthDetections()
         count = count+1
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
         time.sleep(0.1)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(WebcamVideoStream().start()),
+    print time.strftime("%Y-%m-%d %H:%M")
+
+    return Response(gen(SmartCamera().start()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
